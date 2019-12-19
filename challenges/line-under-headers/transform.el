49cf9a893af1bc31ed1057fc430da72f2c4f63a6
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-19 21:08:09 +0200

    `Line under headers` challenge

---
+++ challenges/line-under-headers/transform.el
@@ -0,0 +1,19 @@
+;; http://www.vimgolf.com/challenges/5d7f565deac0df000cfc2154
+
+(defun transform ()
+  (with-temp-file "output"
+    (insert-file-contents "input")
+    (while (not (eobp))
+      (if (looking-at "## ")
+	  (progn
+	    (delete-char 3)
+	    (let ((line-length
+		   (- (line-end-position) (line-beginning-position))))
+	      (forward-line)
+	      (newline)
+	      (forward-line -1)
+	      (insert-char ?- line-length)
+	      (forward-line)))
+	(forward-line)))))
+
+(transform)
