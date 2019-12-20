e00f55f9ccd28df4b2f91f62b2204a78a14398c7
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-20 20:16:05 +0200

    `Simple text editing with vim`

---
+++ challenges/simple-text-editing-with-vim/transform.el
@@ -0,0 +1,13 @@
+(defun transform ()
+  (with-temp-file "output"
+    (insert-file-contents "input")
+    (while (not (eobp))
+      (if (looking-at "# ")
+	  (progn 
+	    (forward-line 2)
+	    (delete-region (line-beginning-position) (line-end-position))
+	    (copy-region-as-kill (line-beginning-position 0) (line-end-position 0))
+	    (yank)))
+	(forward-line))))
+
+(transform)
